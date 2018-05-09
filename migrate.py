6d1e192ad3259eef5695ba534fd969aeae416ac0
zhangxiaofei <1326336909@qq.com>
2018-05-09 14:26:01 +0800

    初始化

---
+++ migrate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+# -------------------------------------------------------------------------
+# 程序：migrate.py
+# 作者：zhangxiaofei
+# 日期：2018-04-27
+# 功能：选项类
+# -------------------------------------------------------------------------
+
+from migrator import Migrator
+from tools.migrate.options import Options
+
+migrate = Migrator('migrates')
+
+# python migrate.py --init
+# python migrate.py --create-migration create_users
+options = Options()
+options.add('-init', migrate.init)
+options.add('-migrate', 'migrate')
+options.add('-rollback', 'rollback')
+options.add('-create', migrate.create)
+options.add('-execute', migrate.execute)
+options.add('-remove', 'remove_migration')
+options.add('-load-schema', 'load_schema')
+options.add('-dump-schema', 'dump_schema')
+options.add('-dump-migration', 'dump_migration')
+
+options.execute()
